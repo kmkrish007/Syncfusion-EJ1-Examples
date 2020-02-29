27a9db369d7554be5fa3680439831842574f4c5b
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-02-29 13:51:10 +0530

    Entity model data sample

---
+++ Support-EJ1/Combobox/MVC/EntityModel/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Entity.Controllers
+{
+    public class HomeController : Controller
+    {
+        ModelMovieContainer db = new ModelMovieContainer();
+        public ActionResult Index()
+        {
+            var dataset = db.Customers.Select(x => new Customer
+                    {
+                        ID = x.CustomerID,
+                        Name = x.ContactName
+                    }).ToList();
+            ViewBag.DataSource = dataset;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class Customer
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
