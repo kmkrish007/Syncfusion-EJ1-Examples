0aeae79327cb59c893a86c7dbecb988f6df726e0
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-02-29 13:45:14 +0530

    Entity model data source from web api combobox

---
+++ Support-EJ1/Combobox/MVC/Entity/EntityDataController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Entity
+{
+    public class EntityDataController : ApiController
+    {
+        ModelMovieContainer db = new ModelMovieContainer();
+        // GET api/<controller>
+        public IEnumerable<Movie> Get()
+        {
+            var dataset = db.Customers.Select(x => new Movie
+            {
+                ID = x.CustomerID,
+                Name = x.ContactName
+            }).ToList();
+
+            return dataset;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
