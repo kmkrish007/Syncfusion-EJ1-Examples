a4b12944f1e58482de9df6cc434313d7aac645b7
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-02-29 14:04:29 +0530

    Treeview samples

---
+++ Support-EJ1/TreeView/WebForms/Treeview/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Treeview._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <ej:Button
+            ID="add"
+            Text="Add Node"
+            Type="Button"
+            runat="server"
+            ClientSideOnClick="Add">
+        </ej:Button>
+    <ej:TreeView 
+            ID="treeview" 
+            runat="server" 
+            DataTextField="Text" 
+            DataIdField="Id" 
+            DataParentIdField="Parent" 
+            DataExpandedField="Expanded">
+        </ej:TreeView>
+
+
+    <script type="text/javascript">
+
+        function Add() {
+            treeObj = $("#<%= treeview.ClientID %>").ejTreeView('instance');
+            var newNode = { Id: 11, Text: "Item 2.1" };
+            //to add tree node
+            treeObj.addNode(newNode, "2");
+        }
+        
+    </script>
+
+</asp:Content>
---
+++ Support-EJ1/TreeView/WebForms/Treeview/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Treeview
+{
+    public partial class _Default : Page
+    {
+        List<LoadData> treeData = new List<LoadData>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            treeData.Add(new LoadData { Id = 1, Parent = 0, Text = "Item 1", Expanded = true });
+            treeData.Add(new LoadData { Id = 2, Parent = 0, Text = "Item 2" });
+            treeData.Add(new LoadData { Id = 3, Parent = 0, Text = "Item 3" });
+            treeData.Add(new LoadData { Id = 4, Parent = 0, Text = "Item 4" });
+            treeData.Add(new LoadData { Id = 5, Parent = 1, Text = "Item 1.1" });
+            treeData.Add(new LoadData { Id = 6, Parent = 1, Text = "Item 1.2" });
+            treeData.Add(new LoadData { Id = 7, Parent = 1, Text = "Item 1.3" });
+            treeData.Add(new LoadData { Id = 8, Parent = 3, Text = "Item 3.1" });
+            treeData.Add(new LoadData { Id = 9, Parent = 3, Text = "Item 3.2" });
+            treeData.Add(new LoadData { Id = 10, Parent = 5, Text = "Item 1.1.1" });
+            this.treeview.DataSource = treeData;
+        }
+    }
+
+    public class LoadData
+    {
+        public int Id { get; set; }
+        public int Parent { get; set; }
+        public string Text { get; set; }
+        public bool Expanded { get; set; }
+    }
+}
