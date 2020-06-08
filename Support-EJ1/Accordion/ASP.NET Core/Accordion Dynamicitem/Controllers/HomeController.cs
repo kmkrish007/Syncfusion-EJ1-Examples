7cf377e6bad0ea2d9b987ebe48b6607fae983950
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-06-08 20:39:20 +0530

    Accordion with uploadbox

---
+++ Support-EJ1/Accordion/ASP.NET Core/Accordion Dynamicitem/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Accordion.Models;
+using Syncfusion.JavaScript.Models;
+using Syncfusion.JavaScript;
+
+namespace Accordion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            AccordionProperties obj = new AccordionProperties();
+            obj.Items.Add(new AccordionBaseItem() { Text = "ASP.NET", ContentTemplate = new MvcTemplate<AccordionBaseItem> { RazorViewTemplate = (data) => { return "Microsoft ASP.NET is a set of technologies in the Microsoft .NET Framework for building Web applications and XML Web services. ASP.NET pages execute on the server and generate markup such as HTML, WML, or XML that is sent to a desktop or mobile browser. ASP.NET pages use a compiled, event-driven programming model that improves performance and enables the separation of application logic and user interface."; } } });
+            obj.Items.Add(new AccordionBaseItem() { Text = "ASP.NET MVC", ContentTemplate = new MvcTemplate<AccordionBaseItem> { RazorViewTemplate = (data) => { return " The Model-View-Controller (MVC) architectural pattern separates an application into three main components: the model, the view, and the controller. The ASP.NET MVC framework provides an alternative to the ASP.NET Web Forms pattern for creating Web applications. The ASP.NET MVC framework is a lightweight, highly testable presentation framework that (as with Web Forms-based applications) is integrated with existing ASP.NET features, such as master pages and membership-based authentication."; } } });
+            obj.Items.Add(new AccordionBaseItem() { Text = "JavaScript", ContentTemplate = new MvcTemplate<AccordionBaseItem> { RazorViewTemplate = (data) => { return "JavaScript (JS) is an interpreted computer programming language."; } } });
+            obj.EnableAnimation = true;
+            ViewData["accordion"] = obj;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Support-EJ1/Accordion/Accordion_UploadBox/WebApplication1VB/App_Start/Utilities.vb
@@ -0,0 +1,29 @@
+﻿Public Class Utilities
+    Public Shared Function Coalesce(ParamArray myvals() As Object) As Object
+        For i As Integer = 0 To myvals.Length - 1
+            If myvals(i) Is Nothing OrElse IsDBNull(myvals(i)) Then Continue For
+            Return myvals(i)
+        Next
+        Return Nothing
+    End Function
+
+    Public Shared Sub ASPNET_Confirm_MsgBox(ByVal Message As String)
+        'JavaScript to display message box if User Name & Password not found
+        System.Web.HttpContext.Current.Response.Write("<SCRIPT LANGUAGE=""JavaScript"">" & vbCrLf)
+        System.Web.HttpContext.Current.Response.Write("var retval = confirm(""" & Message & """)" & vbCrLf)
+        System.Web.HttpContext.Current.Response.Write("</SCRIPT>")
+    End Sub
+    Public Shared Function UploadFile(fileUploadObject As FileUpload, sUserUpload As String, blnActiveFile As Boolean, Optional OverWriteFileID As Integer = 0) As Integer
+        Dim contentType As String = fileUploadObject.PostedFile.ContentType
+        Dim fileData As Byte() = New Byte(fileUploadObject.PostedFile.InputStream.Length - 1) {}
+        fileUploadObject.PostedFile.InputStream.Read(fileData, 0, fileData.Length)
+        Dim fileSize As Long = fileUploadObject.PostedFile.ContentLength
+        Dim originalName As String = IO.Path.GetFileName(fileUploadObject.PostedFile.FileName)
+
+        Dim FileID As Integer
+        'SQL Code goes here to upload the file
+        ' FileID = SimpleSQL.runProcedureScalar("csWebData", "TACTmplt_SaveFiles", FileID, Today, sUserUpload, blnActiveFile, OverWriteFileID, originalName, contentType, fileSize, fileData)
+
+        Return FileID
+    End Function
+End Class
