6f68625a73a0015fbf7531506c36e31a22e27299
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-06-08 20:43:24 +0530

    Accordion SQl sample

---
+++ Support-EJ1/Accordion/WebApplication1/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <ej:Accordion ID="Accordion" runat="server">
+        
+    </ej:Accordion>
+</asp:Content>
---
+++ Support-EJ1/Accordion/WebApplication1/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using Syncfusion.JavaScript.Web;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+                string sql = "SELECT * FROM Categories";
+                SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ToString());
+                SqlCommand cmd = new SqlCommand(sql, myConnection);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count != 0)
+                {
+                    Label lbTitle;
+                    Label lbContent;
+                    AccordionItem acc;
+                    int i = 0;
+
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        lbTitle = new Label();
+                        lbContent = new Label();
+                        lbTitle.Text = dr["CategoryName"].ToString();
+                        lbContent.Text = dr["Description"].ToString();
+                        acc = new AccordionItem();
+                        acc.Text = lbTitle.Text;
+                        acc.ID = "Pane" + i;
+                        acc.ContentSection.Controls.Add(lbContent);
+                        Accordion.Items.Add(acc);
+                        ++i;
+                    }
+                }
+        }
+    }
+}
