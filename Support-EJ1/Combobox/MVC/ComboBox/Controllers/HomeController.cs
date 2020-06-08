4f5c841a895fbe70d63a5fc60d828f1193e2cd9d
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-06-08 21:53:25 +0530

    rightside combobox

---
+++ Support-EJ1/Combobox/MVC/ComboBox/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComboBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<CarsList> car = new List<CarsList>();
+        public ActionResult Index()
+        {
+            car.Add(new CarsList { text = "Audi S6" });
+            car.Add(new CarsList { text = "Austin-Healey" });
+            car.Add(new CarsList { text = "Alfa Romeo" });
+            car.Add(new CarsList { text = "Aston Martin" });
+            car.Add(new CarsList { text = "BMW 7" });
+            car.Add(new CarsList { text = "Bentley Mulsanne" });
+
+            ViewBag.datasource = car;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+    public class CarsList
+    {
+        public string text { get; set; }
+    }
+}
