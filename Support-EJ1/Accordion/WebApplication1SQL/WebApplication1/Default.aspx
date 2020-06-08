852509d57abe83fb071dfa370f2d9ae16e611928
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-06-08 20:41:44 +0530

    Accordion SQl sample

---
+++ Support-EJ1/Accordion/WebApplication1SQL/WebApplication1/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<%@ Register assembly="Syncfusion.EJ.Web, Version=13.4450.0.63, Culture=neutral, PublicKeyToken=3d67ed1f87d44c89" namespace="Syncfusion.JavaScript.Web" tagprefix="ej" %>
+
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1><%: Title %>.</h1>
+                <h2>Modify this template to jump-start your ASP.NET application.</h2>
+            </hgroup>
+            <p>
+                To learn more about ASP.NET, visit <a href="http://asp.net" title="ASP.NET Website">http://asp.net</a>.
+                The page features <mark>videos, tutorials, and samples</mark> to help you get the most from ASP.NET.
+                If you have any questions about ASP.NET visit
+                <a href="http://forums.asp.net/18.aspx" title="ASP.NET Forum">our forums</a>.
+            </p>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+   
+
+
+    <ej:Accordion ID="Accordion" runat="server">
+        
+    </ej:Accordion>
+   
+
+
+</asp:Content>
---
+++ Support-EJ1/Accordion/WebApplication1SQL/WebApplication1/Default.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using Syncfusion.JavaScript.Web;
+using Syncfusion.JavaScript.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadAcc();
+
+            }
+        }
+
+        private void LoadAcc()
+        {
+             string sql = "select * from tbl_Contents";
+             SqlConnection myConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["SQLConnectionString"].ToString());
+             SqlCommand cmd = new SqlCommand(sql, myConnection);
+             SqlDataAdapter da = new SqlDataAdapter(cmd);
+             DataSet ds = new DataSet();
+             da.Fill(ds);
+
+             if (ds.Tables[0].Rows.Count != 0)
+             {
+                 Label lbTitle;
+                 Label lbContent;
+                 AccordionItem acc;
+                 int i = 0;
+                
+                 foreach (DataRow dr in ds.Tables[0].Rows)
+                 {
+                     lbTitle = new Label();
+                     lbContent = new Label();
+                     lbTitle.Text = dr["Title"].ToString();
+                     lbContent.Text = dr["Detail"].ToString();
+                     acc = new AccordionItem();
+                     acc.Text = lbTitle.Text;
+                     acc.ID = "Pane" + i;
+                     acc.ContentSection.Controls.Add(lbContent);
+                     Accordion.Items.Add(acc);
+                     ++i;
+                 }
+             }
+        }
+
+      
+    }
+}
