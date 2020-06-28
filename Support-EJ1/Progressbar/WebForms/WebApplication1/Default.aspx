b17c7b3f383d81773615269a2f7fffdb6720fba9
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-06-28 21:27:29 +0530

    Progressbar

---
+++ Support-EJ1/Progressbar/WebForms/WebApplication1/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div id="ControlDIv" runat="server"></div>
+
+</asp:Content>
---
+++ Support-EJ1/Progressbar/WebForms/WebApplication1/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Syncfusion.JavaScript.Web.ProgressBar Progress = new Syncfusion.JavaScript.Web.ProgressBar();
+            Progress.Percentage = 70;
+            Progress.Text = "70 %";
+            Progress.MaxValue = 100;
+            Progress.Height = "25px";
+            Progress.Width = "500px";
+            ControlDIv.Controls.Add(Progress);
+        }
+    }
+}
