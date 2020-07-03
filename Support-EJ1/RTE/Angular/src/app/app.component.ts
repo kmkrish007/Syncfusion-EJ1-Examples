447897218f1aedd5e11a8c3b883d1bbe33007718
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-07-03 13:25:25 +0530

    Angular RTE

---
+++ Support-EJ1/RTE/Angular/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, ViewEncapsulation, ViewChild} from '@angular/core';
+import { EJComponents } from 'ej-angular2/src/ej/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+
+
+export class AppComponent {
+  constructor() {
+  }
+}
