e1126eb7e50e9e0389a363447b2848fa902820b0
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-07-03 22:52:14 +0530

    RTE

---
+++ Support-EJ1/RTE/MVC/ImageBrowser/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Syncfusion.JavaScript;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FileActionMethods(FileExplorerParams args)
+        {
+            FileExplorerOperations opeartion = new FileExplorerOperations();
+            switch (args.ActionType)
+            {
+                case "Read":
+                    return Json(opeartion.Read(args.Path, args.ExtensionsAllow));
+                case "CreateFolder":
+                    return Json(opeartion.CreateFolder(args.Path, args.Name));
+                case "Paste":
+                    return Json(opeartion.Paste(args.LocationFrom, args.LocationTo, args.Names, args.Action, args.CommonFiles));
+                case "Remove":
+                    return Json(opeartion.Remove(args.Names, args.Path));
+                case "Rename":
+                    return Json(opeartion.Rename(args.Path, args.Name, args.NewName, args.CommonFiles));
+                case "GetDetails":
+                    return Json(opeartion.GetDetails(args.Path, args.Names));
+                case "Download":
+                    opeartion.Download(args.Path, args.Names);
+                    break;
+                case "Upload":
+                    opeartion.Upload(args.FileUpload, args.Path);
+                    break;
+                case "Search":
+                    return Json(opeartion.Search(args.Path, args.ExtensionsAllow, args.SearchString, args.CaseSensitive));
+            }
+            return Json("");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Support-EJ1/RTE/MVC/RTE_Video/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.EJ.Export;
+using Syncfusion.Pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
