87e6cc8eaecb26f8bf4aa5afc409b1b1162af6cc
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-07-03 22:12:04 +0530

    ComboBox_multicolumn data/Default.aspx

---
+++ Support-EJ1/Combobox/WebForms/ComboBox_multicolumn data/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ComboBox._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    
+
+ <ej:ComboBox ID="cboTerapija" runat="server"
+        Width="100%"
+        DataTextField="CompanyName"
+        DataValueField="CustomerID"
+        DataSourceID="SqlDataSourceTerapija"
+        Placeholder="Odaberite terapiju"
+        ClientSideOnChange="myFunctionOnChange"
+        ItemTemplate="<span><span class='name'>${CustomerID}</span><span class ='city'>${CompanyName}</span><span class ='city'>${City}</span></span>">
+</ej:ComboBox>
+
+        <asp:SqlDataSource ID="SqlDataSourceTerapija" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT TOP 10 [CustomerID], [CompanyName], [City] FROM [Customers]" ></asp:SqlDataSource>
+
+
+    <script type="text/javascript">
+
+        function myFunctionOnChange(args) {
+            console.log("Customer ID: " + args.itemData.CustomerID);
+            console.log("CompanyName: " + args.itemData.CompanyName);
+            console.log("City: " + args.itemData.City);
+        }
+        
+    </script>
+
+
+</asp:Content>
---
+++ Support-EJ1/Combobox/WebForms/ComboBox_multicolumn data/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComboBox
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Employee> Data = new List<Employee>();
+            List<CarsList> car = new List<CarsList>();
+            Data.Add(new Employee { Text = "Erik Linden" });
+            Data.Add(new Employee { Text = "John Linden" });
+            Data.Add(new Employee { Text = "Louis" });
+            Data.Add(new Employee { Text = "Lawrence" });
+            //givingTo.DataSource = Data;
+
+            car.Add(new CarsList { type = "Audi S6" });
+            car.Add(new CarsList { type = "Austin-Healey" });
+            car.Add(new CarsList { type = "Alfa Romeo" });
+            car.Add(new CarsList { type = "Aston Martin" });
+            car.Add(new CarsList { type = "BMW 7" });
+            car.Add(new CarsList { type = "Bentley Mulsanne" });
+            car.Add(new CarsList { type = "Bugatti Veyron" });
+            car.Add(new CarsList { type = "Chevrolet Camaro" });
+            car.Add(new CarsList { type = "Cadillac" });
+            car.Add(new CarsList { type = "Duesenberg J" });
+            car.Add(new CarsList { type = "Dodge Sprinter" });
+            car.Add(new CarsList { type = "Elantra" });
+            //type1.DataSource = car;
+        }
+
+        protected void sub2HR_Click1(object sender, Syncfusion.JavaScript.Web.ButtonClickEventArgs e)
+        {
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Text { get; set; }
+        public string Role { get; set; }
+        public string Country { get; set; }
+    }
+
+    public class CarsList
+    {
+        public string type { get; set; }
+    }
+}
