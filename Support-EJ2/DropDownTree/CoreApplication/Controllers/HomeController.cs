98356ec8f0f01f942f13a0dc8127e52482741a6e
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-11-10 12:52:11 +0530

    DD Tree core sample

---
+++ Support-EJ2/DropDownTree/CoreApplication/Controllers/HomeController.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreApplication.Models;
+
+namespace CoreApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public IActionResult SaveData()
+        {
+            // treedata is the name of dropdowntree
+            var value = HttpContext.Request.Form["treedata"];
+            return View();
+        }
+        public IActionResult Index()
+        {
+            List<object> treedata = new List<object>();
+            treedata.Add(new
+            {
+                id = 1,
+                name = "Australia",
+                hasChild = true,
+                expanded = true
+            });
+            treedata.Add(new
+            {
+                id = 2,
+                pid = 1,
+                name = "New South Wales",
+
+            });
+            treedata.Add(new
+            {
+                id = 3,
+                pid = 1,
+                name = "Victoria"
+            });
+
+            treedata.Add(new
+            {
+                id = 4,
+                pid = 1,
+                name = "South Australia"
+            });
+            treedata.Add(new
+            {
+                id = 6,
+                pid = 1,
+                name = "Western Australia",
+
+            });
+            treedata.Add(new
+            {
+                id = 7,
+                name = "Brazil",
+                hasChild = true
+            });
+            treedata.Add(new
+            {
+                id = 8,
+                pid = 7,
+                name = "Paraná"
+            });
+            treedata.Add(new
+            {
+                id = 9,
+                pid = 7,
+                name = "Ceará"
+            });
+            treedata.Add(new
+            {
+                id = 10,
+                pid = 7,
+                name = "Acre"
+            });
+            treedata.Add(new
+            {
+                id = 11,
+                name = "China",
+                hasChild = true
+            });
+            treedata.Add(new
+            {
+                id = 12,
+                pid = 11,
+                name = "Guangzhou"
+            });
+            treedata.Add(new
+            {
+                id = 13,
+                pid = 11,
+                name = "Shanghai"
+            });
+            treedata.Add(new
+            {
+                id = 14,
+                pid = 11,
+                name = "Beijing"
+            });
+            treedata.Add(new
+            {
+                id = 15,
+                pid = 11,
+                name = "Shantou"
+
+            });
+            treedata.Add(new
+            {
+                id = 16,
+                name = "France",
+                hasChild = true
+
+            });
+            treedata.Add(new
+            {
+                id = 17,
+                pid = 16,
+                name = "Pays de la Loire"
+
+            });
+            treedata.Add(new
+            {
+                id = 18,
+                pid = 16,
+                name = "Aquitaine"
+
+            });
+            treedata.Add(new
+            {
+                id = 19,
+                pid = 16,
+                name = "Brittany"
+
+            });
+            treedata.Add(new
+            {
+                id = 20,
+                pid = 16,
+                name = "Lorraine"
+            });
+            treedata.Add(new
+            {
+                id = 21,
+                name = "India",
+                hasChild = true
+
+            });
+            treedata.Add(new
+            {
+                id = 22,
+                pid = 21,
+                name = "Assam"
+
+            });
+            treedata.Add(new
+            {
+                id = 23,
+                pid = 21,
+                name = "Bihar"
+            });
+            treedata.Add(new
+            {
+                id = 24,
+                pid = 21,
+                name = "Tamil Nadu"
+
+            });
+            ViewBag.dataSource = treedata;
+            ViewBag.checkedNodes = new string[] { "1" };
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
