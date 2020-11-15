d824a2d842791f57b73979ac79a350fd484fd656
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-11-15 20:46:40 +0530

    upload

---
+++ Support-EJ1/UploadBox/Core/WebApplication1/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private IHostingEnvironment hostingEnv;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            this.hostingEnv = env;
+        }
+        public IActionResult UploadFiles()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFiles(IList<IFormFile> UploadDefault)
+        {
+            var files = HttpContext.Request.Form.Files;
+            long size = 0;
+            foreach (var file in files)
+            {
+                var filename = ContentDispositionHeaderValue
+                                .Parse(file.ContentDisposition)
+                                .FileName
+                                .Trim('"');
+                filename = hostingEnv.WebRootPath + $@"\{filename}";
+                size += file.Length;
+                using (FileStream fs = System.IO.File.Create(filename))
+                {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+            return Content("");
+        }
+    }
+}
