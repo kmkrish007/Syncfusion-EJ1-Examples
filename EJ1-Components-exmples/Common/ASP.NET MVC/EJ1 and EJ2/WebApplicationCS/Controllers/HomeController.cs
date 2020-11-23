75db0ab3f6c872257212ce48999d1d314fe6f5df
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-11-23 10:57:45 +0530

    Ej1 and Ej2 in mvc

---
+++ EJ1-Components-exmples/Common/ASP.NET MVC/EJ1 and EJ2/WebApplicationCS/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationCS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<SkillSet> skill = new List<SkillSet>();
+            skill.Add(new SkillSet { text = "ASP.NET" });
+            skill.Add(new SkillSet { text = "ActionScript" });
+            skill.Add(new SkillSet { text = "Basic" });
+            skill.Add(new SkillSet { text = "C++" });
+            skill.Add(new SkillSet { text = "C#" });
+            skill.Add(new SkillSet { text = "dBase" });
+            skill.Add(new SkillSet { text = "Delphi" });
+            skill.Add(new SkillSet { text = "ESPOL" });
+            skill.Add(new SkillSet { text = "F#" });
+            skill.Add(new SkillSet { text = "FoxPro" });
+            skill.Add(new SkillSet { text = "Java" });
+            skill.Add(new SkillSet { text = "J#" });
+            skill.Add(new SkillSet { text = "Lisp" });
+            skill.Add(new SkillSet { text = "Logo" });
+            skill.Add(new SkillSet { text = "PHP" });
+            ViewBag.datasource = skill;
+            ViewBag.data = new string[] { "BadmHennessey Venominton", "Bugatti Chiron", "Bugatti Veyron Super Sport", "SSC Ultimate Aero", "Koenigsegg CCR", "McLaren F1", "Aston Martin One- 77", "Jaguar XJ220" };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class SkillSet
+    {
+        public string text { get; set; }
+    }
+}
---
+++ EJ1-Components-exmples/DropDownList/WebForms/DropdownlistVB_SelectedIndices/Default.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports Syncfusion.JavaScript.Web
+Public Class _Default
+    Inherits Page
+    Dim ctr As Control
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        Dim groups As List(Of GroupsList) = New List(Of GroupsList)()
+        groups.Add(New GroupsList("a", "Group A"))
+        groups.Add(New GroupsList("b", "Group B"))
+        groups.Add(New GroupsList("c", "Group C"))
+        groups.Add(New GroupsList("d", "Group D"))
+        groups.Add(New GroupsList("e", "Group E"))
+        ddlgroupsList.DataSource = groups
+        ddlgroupsList.SelectedIndices = New List(Of Integer)({0, 1, 2})
+    End Sub
+
+    <Serializable>
+    Class GroupsList
+        Public Property parentId As String
+        Public Property text As String
+
+        Public Sub New(ByVal gID As String, ByVal gtext As String)
+            Me.parentId = gID
+            Me.text = gtext
+        End Sub
+    End Class
+
+End Class
