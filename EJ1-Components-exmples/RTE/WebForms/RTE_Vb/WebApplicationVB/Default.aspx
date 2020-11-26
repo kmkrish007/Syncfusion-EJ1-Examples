fba9d8ba854dd6556d6d898d57d69802bdf89530
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-11-26 13:17:11 +0530

    RTE Image browser VB

---
+++ EJ1-Components-exmples/RTE/WebForms/RTE_Vb/WebApplicationVB/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplicationVB._Default" %>
+
+<%@ Register assembly="Syncfusion.EJ.Web" namespace="Syncfusion.JavaScript.Web" tagprefix="ej" %>
+<asp:Content ID="Content1" runat="server" contentplaceholderid="MainContent">
+
+     <ej:RTE ID="RTE1" ToolsList="images" runat="server">
+    <RTEContent>
+            Description:
+            <p> The Rich Text Editor (RTE) control is an easy to render in
+            client side. Customer easy to edit the contents and get the HTML content for
+            the displayed content. A rich text editor control provides users with a toolbar
+            that helps them to apply rich text formats to the text entered in the text
+            area. </p>
+    </RTEContent>
+    <Tools Images="image"> </Tools>
+    <ImageBrowser ExtensionAllow="*.png,*.gif,*.jpg,*.jpeg" FilePath="~/FileBrowser/"  AjaxAction="Default.aspx/FileActionDefault"/>
+    <FileBrowser ExtensionAllow="*.png,*.txt,*.jpg,*.docx" FilePath="~/FileBrowser/" AjaxAction="Default.aspx/FileActionDefault"/>
+</ej:RTE>
+</asp:Content>
+
---
+++ EJ1-Components-exmples/RTE/WebForms/RTE_Vb/WebApplicationVB/Default.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Data.SqlClient
+Imports Syncfusion.JavaScript
+
+Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    <System.Web.Services.WebMethod>
+    Public Shared Function FileActionDefault(ByVal ActionType As String, ByVal Path As String, ByVal ExtensionsAllow As String, ByVal LocationFrom As String, ByVal LocationTo As String, ByVal Name As String, ByVal Names As String(), ByVal NewName As String, ByVal Action As String, ByVal CommonFiles As IEnumerable(Of CommonFileDetails)) As Object
+        Dim operation As FileExplorerOperations = New FileExplorerOperations()
+
+        Select Case ActionType
+            Case "Read"
+                Return (operation.Read(Path, ExtensionsAllow))
+            Case "CreateFolder"
+                Return (operation.CreateFolder(Path, Name))
+            Case "Paste"
+                operation.Paste(LocationFrom, LocationTo, Names, Action, CommonFiles)
+            Case "Remove"
+                operation.Remove(Names, Path)
+            Case "Rename"
+                operation.Rename(Path, Name, NewName, CommonFiles)
+            Case "GetDetails"
+                Return (operation.GetDetails(Path, Names))
+        End Select
+
+        Return ""
+    End Function
+End Class
