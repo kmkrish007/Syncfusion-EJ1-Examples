84615b715b8160d6ff4eaba691510ab9897ebda6
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2020-12-31 11:41:50 +0530

    signature code behind

---
+++ EJ1-Components-exmples/Signature/WebForms/Signature CodeBehind/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Syncfusion.JavaScript.Web.Signature sign = new Syncfusion.JavaScript.Web.Signature();
+            sign.StrokeWidth = 3;
+            sign.Height = "400px";
+            sign.IsResponsive = true;
+            ControlDIv.Controls.Add(sign);
+        }
+    }
+}
