71222d123d104a29e4475ae4d73cdecae0d5af3e
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-01-07 18:21:52 +0530

    Core File Explorer

---
+++ EJ1-Components-exmples/FileExplorer/ASP.NET Core/Core_FE/Core_FE/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Core_FE.Models;
+using Syncfusion.JavaScript;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Core_FE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public FileExplorerOperations operation;
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            operation = new FileExplorerOperations(hostingEnvironment.ContentRootPath);
+        }
+        public ActionResult FileActionDefault([FromBody] FileExplorerParams args)
+        {
+            switch (args.ActionType)
+            {
+                case "Read":
+                    return Json(operation.Read(args.Path, args.ExtensionsAllow));
+                case "CreateFolder":
+                    return Json(operation.CreateFolder(args.Path, args.Name));
+                case "Paste":
+                    return Json(operation.Paste(args.LocationFrom, args.LocationTo, args.Names, args.Action, args.CommonFiles));
+                case "Remove":
+                    return Json(operation.Remove(args.Names, args.Path, args.SelectedItems));
+                case "Rename":
+                    return Json(operation.Rename(args.Path, args.Name, args.NewName, args.CommonFiles));
+                case "GetDetails":
+                    return Json(operation.GetDetails(args.Path, args.Names));
+                case "Search":
+                    return Json(operation.Search(args.Path, args.ExtensionsAllow, args.SearchString, args.CaseSensitive));
+            }
+            return Json("");
+        }
+
+        public ActionResult Download(FileExplorerParams args)
+        {
+            return operation.Download(args.Path, args.Names);
+        }
+
+        public ActionResult Upload(FileExplorerParams args)
+        {
+            operation.Upload(args.FileUpload, args.Path);
+            return Json("");
+        }
+        public ActionResult GetImage(FileExplorerParams args)
+        {
+            return operation.GetImage(args.Path);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EJ1-Components-exmples/FileExplorer/ASP.NET Core/Core_FE/Core_FE/wwwroot/FileBrowser/NewFolder/Default.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplicationVB._Default" %>
+
+<%@ Register assembly="Syncfusion.EJ.Web" namespace="Syncfusion.JavaScript.Web" tagprefix="ej" %>
+<asp:Content ID="Content1" runat="server" contentplaceholderid="MainContent">
+    
+    <ej:TreeView
+            ID="TreeXmlDS"
+            runat="server"
+        CssClass="custom"
+            DataSourceID="XmlDataSource1"
+        ClientSideOnCreated="onCreated">
+        </ej:TreeView>
+    
+        <div>
+            <asp:XmlDataSource
+                ID="XmlDataSource1"
+                runat="server"
+                DataFile="~\App_Data\XMLData.xml">
+            </asp:XmlDataSource>
+        </div>
+
+    <script>
+        function onCreated(args) {
+            debugger;
+        }
+    </script>
+    <style>
+        .custom.e-treeview .e-item {
+            /*background-image: url(Content//icons//file_icons.png);*/
+            background-image: url(Content//icons//folder.png);
+            background-repeat: no-repeat;
+            /*background-position: 5px -548px;*/
+            background-position: 12px 0px;
+        }
+        .e-treeview .e-text{
+            left: 18px;
+    position: relative;
+        }
+
+        .e-text-wrap {
+    height: 25px;
+}
+        
+    </style>
+</asp:Content>
+
