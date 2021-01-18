8a1e4d4099cdc9a28887f9ab2afc2824dd353a79
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-01-18 17:40:14 +0530

    Get selected value

---
+++ EJ1-Components-exmples/Combobox/WebForms/Get selected value/Default.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ComboBox._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+     <asp:Label runat="server" AssociatedControlID="txtCustID" CssClass="control-label" Style="font-size: 12px; color: black;">Customer Name</asp:Label>
+     <ej:ComboBox ID="txtCustID" ClientIDMode="Static" runat="server" Width="100%"
+           DataTextField="type" DataValueField="custid" ClientSideOnSelect="txtCustID_onSelect"  Placeholder="Select Customer">
+ </ej:ComboBox>
+
+    <script>
+         function  txtCustID_onSelect(args) {
+             var custid = args.value;
+                alert(custid);
+              return;
+         }
+    </script>
+</asp:Content>
---
+++ EJ1-Components-exmples/Combobox/WebForms/Get selected value/Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ComboBox
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<CarsList> car = new List<CarsList>();
+            car.Add(new CarsList { type = "Audi S6", custid="1" });
+            car.Add(new CarsList { type = "Austin-Healey", custid = "2" });
+            car.Add(new CarsList { type = "Alfa Romeo", custid = "3" });
+            car.Add(new CarsList { type = "Aston Martin", custid = "4" });
+            car.Add(new CarsList { type = "BMW 7", custid = "5" });
+            car.Add(new CarsList { type = "Bentley Mulsanne", custid = "6" });
+            car.Add(new CarsList { type = "Bugatti Veyron", custid = "7" });
+            car.Add(new CarsList { type = "Chevrolet Camaro", custid = "8" });
+            car.Add(new CarsList { type = "Cadillac", custid = "9" });
+            car.Add(new CarsList { type = "Duesenberg J", custid = "10" });
+            car.Add(new CarsList { type = "Dodge Sprinter", custid = "11" });
+            car.Add(new CarsList { type = "Elantra", custid = "12" });
+            txtCustID.DataSource = car;
+        }
+    }
+    public class CarsList
+    {
+        public string type { get; set; }
+        public string custid { get; set; }
+    }
+}
