c9dd26f240252ee82065fe443a3d1ba60c90708a
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-02-01 17:05:09 +0530

    Changes

--- EJ1-Components-exmples/FileExplorer/Angular/File Explorer/src/app/app.component.ts
+++ EJ1-Components-exmples/FileExplorer/Angular/File Explorer/src/app/app.component.ts
@@ -11,31 +11,17 @@ import { EJComponents } from 'ej-angular2/src/ej/core';
 
 
 export class AppComponent {
-  contextMenuSettings: Object;
   @ViewChild('file') fileObj: EJComponents<any, any>;
-  constructor() {
-    this.contextMenuSettings = {
-      // define the ContextMenu items
-      items: {
-          // removed the 'NewFolder' item from NavigationPane ContextMenu
-          navbar: ['Custom', 'Upload', '|', 'Delete', 'Rename', '|', 'Cut', 'Copy', 'Paste', '|', 'Getinfo'],
-          // added the custom ContextMenu item (View) to Current working directory ContextMenu
-          cwd: ['Refresh', 'Paste', '|', 'SortBy', '|', 'NewFolder', 'Upload', '|', 'Getinfo'],
-          // removed 'Upload' item from Selected files/ folder's ContextMenu
-          files: ['Custom','Open', 'Download', '|', 'Delete', 'Rename', '|', 'Cut', 'Copy', 'Paste', '|','OpenFolderLocation', 'Getinfo']
-      },
-      // added the custom ContextMenu item's (View) functionality
-      customMenuFields: [
-      {
-          id: 'Custom',
-          text: 'Get Path',
-          spriteCssClass: 'custom-grid'
-      }, ]
-  };
-
-  }
-  onMenuClick(args) {
-    if(args.text == "Get Path")
-      console.log("Path :" + this.fileObj.widget._originalPath + this.fileObj.widget._selectedItems[0]);
+  ngAfterViewInit(){
+    // for gridview
+    if(this.fileObj.widget._gridObj){
+      this.fileObj.widget._gridObj._scrollObject.setModel({ scrollerSize: 16 });
     }
+    // for tile view
+    if(this.fileObj.widget._tileScroll) {
+      this.fileObj.widget._tileScroll.setModel({ scrollerSize: 16 });
+    }
+  }
+  constructor() {
+  };
 }
